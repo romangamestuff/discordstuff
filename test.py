5ae680f4a580b9bf3e11302078b320be239faff8
romangamestuff <roman.cordova719@gmail.com>
2026-03-30 21:53:06 -0600

    Add webhook post request to Discord

---
+++ test.py
@@ -0,0 +1,5 @@
+import requests
+
+requests.post("https://discord.com/api/webhooks/1488385093964791830/vVKGJmvUXvnEXJinDqGrfMlDnMUzABs79kx3-SC3d0ITOV3c6KOZ3B2A3yShofPIwJoM", json={
+    "content": "🚀 Ub3r Bot is connected"
+})
